9346e6d636d0b563854c66fe3736db9ced64e299
saintclair2025 <w0873771@myscc.ca>
2025-11-20 20:20:47 -0500

    Create frontend.jsx
    added reactjs file for the med link application

---
+++ frontend.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Med Link</h1>
+      <p>Pharmacy Communication & Refill Management System</p>
+    </div>
+  );
+}
+
+export default App;
